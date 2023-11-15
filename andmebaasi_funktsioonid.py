2f8a4baacc21cf45942f13878b56ef65a220e82b
RobinHenrik <robin.neem@gmail.com>
2023-11-15 16:37:36 +0200

    andmebaasi lisamine valmis. Uks fail andmebaasi funktsioonidega. Kogemata olin terve aeg main branchis

--- andmebaasi_funktsioonid.py
+++ andmebaasi_funktsioonid.py
@@ -1,4 +1,24 @@
-from toidud_sonastikku import toidud_sõnastikku
+def toidud_sõnastikku():
+    f = open("toiduandmed.csv", "r", encoding="utf-8")
+    toiduandmed = {}
+    for rida in f:
+        rida = rida.strip("\n")
+        rida = rida.split(",")
+        toidu_nimi = rida[0].lower()
+        kcal = float(rida[1])
+        susivesikud = float(rida[2])
+        rasvad = float(rida[3])
+        kiudained = float(rida[4])
+        valgud = float(rida[5])
+        suhkrud = float(rida[6])
+        toiduandmed[toidu_nimi] = {}
+        toiduandmed[toidu_nimi]['kcal'] = kcal
+        toiduandmed[toidu_nimi]['süsivesikud'] = susivesikud
+        toiduandmed[toidu_nimi]['suhkrud'] = suhkrud
+        toiduandmed[toidu_nimi]['rasvad'] = rasvad
+        toiduandmed[toidu_nimi]['valgud'] = valgud
+        toiduandmed[toidu_nimi]['kiudained'] = kiudained
+    return toiduandmed
 
 def lisa_andmebaasi():
     toit = input("Sisesta toit, mida soovid andmebaasi lisada: ").capitalize()
@@ -57,4 +77,3 @@ def lisa_andmebaasi():
     f.write(f"{toit},{kcal},{susivesikud},{rasvad},{kiudained},{valgud},{suhkrud}\n")
     f.close()
 
-lisa_andmebaasi()
