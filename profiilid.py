06d0933fb94e37fa8d38151384c9a734d25739df
Marten221 <144438767+Marten221@users.noreply.github.com>
2023-11-15 15:03:05 +0200

    bmi

--- profiilid.py
+++ profiilid.py
@@ -7,18 +7,21 @@ def faili_lugemine():
         for rida in f:
             if rida.strip() != '':
                 andmed.append(rida.strip().split(','))
-    return andmed
 
+    for jar in andmed:                #TEEN KÕIK ANDMED FAILIS PEALE NIME FLOATIDEKS
+        for i in range(1, len(jar)):
+            jar[i] = float(jar[i])
+    return andmed
 
 
 def profiili_loomine():
 
     nimi = input("Sisestage oma nimi: ")
-    vanus = int(input("Sisestage oma vanus: "))
-    kaal = float(input("Sisestage oma kehakaal: "))
-    pikkus = int(input("Sisestage oma pikkus: "))
+    vanus = float(input("Sisestage oma vanus: "))
+    kaal = float(input("Sisestage oma kehakaal(kg): "))
+    pikkus = float(input("Sisestage oma pikkus(cm): "))
     with open("profiilid.txt", "a", encoding="utf-8") as f:
-        f.write(f"{nimi},{vanus},{kaal},{pikkus}\n")
+        f.write(f"{nimi},{vanus},{kaal},{pikkus},{round(kaal / ((pikkus/100) ** 2), 2)}\n") #LISAN FAILI PROFIILI: NIMI,VANUS,KAAL,PIKKUS,BMI
     andmed = faili_lugemine()
 
 
@@ -43,7 +46,7 @@ def profiili_kustutamine(kustutatav): #PROFIILIDE KUSTUTAMINE
     with open('profiilid.txt', "w", encoding='utf-8') as f:
         for i in range(len(andmed)):
             andmed[i] = andmed[i].strip().split(",")
-            if andmed[i][0] != kustutatav:
+            if andmed[i][0].lower() != kustutatav.lower():
                 f.write(",".join(andmed[i]) + "\n")
             else:
                 kustutatud += 1
@@ -86,12 +89,14 @@ while tegu != "c":
         profiili_loomine()
         print("Profiil loodud!")
         #andmed = faili_lugemine()
+
     elif tegu == "b": #profiili muutmine
         prindi_profiilinimed()
         muudetav = input("Muudetava profiili nimi: ")
         sobis = profiili_muutmine(muudetav)
         #if sobis == False:
             #print("Profiil antud nimega puudub, äkki sisestasid profiilinime valesti?")
+
     elif tegu == "x": #profiili kustutamine
         prindi_profiilinimed()
         kustutatav = input("Sisesta kustutatava profiili nimi(C kui soovid tühistada): ").lower()
@@ -103,8 +108,10 @@ while tegu != "c":
             else:
                 print("Profiil edukalt kustutatud")
                 time.sleep(1)
+
     elif tegu == "c":
         break
+
     else:
         print("Peate sisestama A, B, C või C")
 
